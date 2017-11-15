8cc3dd36a357795b9cc45cade42d25331ee65498
kjw <kishe56@gmail.com>
2017-11-15 16:40:53 +0900

    first commit

---
+++ app/src/premium/java/com.kmlwriter.kjw.myway/TestModel.java
@@ -0,0 +1,16 @@
+package com.kmlwriter.kjw.myway;
+
+/**
+ * Created by kjw on 2017. 11. 15..
+ */
+
+public class TestModel {
+    private String test;
+
+    public TestModel() {
+        this.test = "premium";
+    }
+    public String getTest() {
+        return test;
+    }
+}
