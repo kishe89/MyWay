dc6a55b448f8670a250dbb6a02be159565079c73
kjw <kishe56@gmail.com>
2017-12-06 03:27:33 +0900

    import optimize

--- app/src/main/java/com/kmlwriter/kjw/myway/ContainerActivity.java
+++ app/src/main/java/com/kmlwriter/kjw/myway/ContainerActivity.java
@@ -3,18 +3,13 @@ package com.kmlwriter.kjw.myway;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.airbnb.lottie.LottieAnimationView;
-
-import butterknife.BindView;
 
 public class ContainerActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener, TabLayout.OnTabSelectedListener {
 
